c78691360c81b7a89d9e9ad4cf6b7c313516294f
Banner Gonzales <bannergz1999@gmail.com>
2016-04-16 13:37:16 -0500

    holas

---
+++ CLASES/Clase1/Anotaciones/src/main/java/demo02/Mate.java
@@ -0,0 +1,11 @@
+package demo02;
+import javax.inject.Named;
+
+@Named
+public class Mate {
+	
+  public int Sumar(int n1,int n2){
+	  return (n1 + n2);
+	  
+  }
+}
--- CLASES/Clase1/Anotaciones/src/main/java/demo02/PruebaA.java
+++ CLASES/Clase1/Anotaciones/src/main/java/demo02/PruebaA.java
@@ -1,4 +1,4 @@
-package demo01;
+package demo02;
 
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -6,14 +6,14 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class PruebaA {
   @SuppressWarnings("resource")
 public static void main(String[] args) {
-	  String fileXML = "demo01/demo01.xml";
+	  String fileXML = "demo02/demo01.xml";
 	BeanFactory bf; //Bean Factory
 	bf = new ClassPathXmlApplicationContext(fileXML);
 	// Acceso de obj 
 	
 	Mate bean;
 	//bean = bf.getBean(Mate.class);
-	bean = bf.getBean("Mate", Mate.class);
+	bean = bf.getBean("mate", Mate.class);
 	
 	//Prueba
 	System.out.println("7+4 = " + bean.Sumar(7, 4));
